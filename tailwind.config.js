b8d9a4c8ddcbad905cc26eeb4ada2531a23b608f
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-08-07 23:07:08 +0200

    rewinit project to use vite + nx
    Signed-off-by: Théo Tchilinguirian <theo.tchlx@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
